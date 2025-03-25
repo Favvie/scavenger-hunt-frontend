1323e5eba0532556c667be50a42fc1d8e4eaf6db
olamide531 <peacefadahunsi676@gmail.com>
2025-03-25 07:06:10 -0700

    worked on the Feature Section for homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import HeroSection from "@/components/homepage/HeroSection";
 import FeatureChallenges from "../components/homepage/FeaturedChallenges";
+import FeatureSection from "@/components/homepage/FeatureSection";
 
 export default function Home() {
   return (
     <>
       <div>
-        <HeroSection />
+        <HeroSection  />
+        
+        <FeatureSection />
         <div className="w-full min-h-full bg-black pb-[40px]" >
           <FeatureChallenges />
         </div>
