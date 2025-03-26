e28e44f265ce50ed83ee865e6ea7d3af171a2570
Verifieddanny <dannyclassic56@gmail.com>
2025-03-27 00:51:22 +0100

    Feat: Implement Challenges Dashboard with Functional Tabs

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -11,10 +11,10 @@ export default function DashboardLayout({children}) {
    return (
       <main className="bg-[#060B1C]">
         <Navbar />
-        <div className="flex h-screen overflow-hidden">
+        <div className="flex min-h-screen overflow-hidden">
             <Sidebar />
-            <div className="md:ml-60">
-                <main className="text-white px-5">{children}</main>
+            <div className="md:ml-60 w-full">
+                <main className="text-white px-5 w-full">{children}</main>
             </div>
         </div>
       </main>
